757431a53d099ba58da37b528facc2598fc7cd4b
cristian <cchura.cpc@gmail.com>
2025-10-07 19:46:57 -0400

    routerLink eliminado

--- src/app/web/inicio/inicio.ts
+++ src/app/web/inicio/inicio.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.scss'
 })
