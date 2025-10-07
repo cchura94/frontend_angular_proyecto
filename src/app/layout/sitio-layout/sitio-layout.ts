13807e84c553a40d099afc2bdd3cd7683a072a46
cristian <cchura.cpc@gmail.com>
2025-10-06 20:26:56 -0400

    diseño sitio web

---
+++ src/app/layout/sitio-layout/sitio-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-sitio-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './sitio-layout.html',
+  styleUrl: './sitio-layout.scss'
+})
+export class SitioLayout {
+
+}
