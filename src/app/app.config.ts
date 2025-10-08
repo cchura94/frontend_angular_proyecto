7280b57bd2740e3a15be479fe3072a31e0759445
cristian <cchura.cpc@gmail.com>
2025-10-07 20:59:55 -0400

    template dashboard admin reduciendo la version v20 de angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptorInterceptor } from './core/interceptors/auth-interceptor-interceptor';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: 'Aura', options: { darkModeSelector: '.app-dark' } } }),
     provideHttpClient(withInterceptors([authInterceptorInterceptor]), withFetch())
   ]
 };
