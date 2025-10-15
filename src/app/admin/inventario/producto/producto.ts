f4a0f805058111146ef90a354cdc0a6400cb0ff8
cristian <cchura.cpc@gmail.com>
2025-10-15 19:46:57 -0400

    Productos creación

--- src/app/admin/inventario/producto/producto.ts
+++ src/app/admin/inventario/producto/producto.ts
@@ -5,10 +5,17 @@ import { Table, TableModule } from 'primeng/table';
 import { ToastModule } from 'primeng/toast';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ProductoService } from '../../../core/services/producto.service';
+import { DialogModule } from 'primeng/dialog';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { CategoriaService } from '../../../core/services/categoria.service';
 
 @Component({
   selector: 'app-producto',
-  imports: [ToastModule, ToolbarModule, ButtonModule, TableModule, CommonModule],
+  imports: [ToastModule, ToolbarModule, ButtonModule, TableModule, CommonModule, DialogModule, ReactiveFormsModule, RadioButtonModule, InputNumberModule, InputTextModule, TextareaModule],
   templateUrl: './producto.html',
   styleUrl: './producto.scss',
 })
@@ -16,30 +23,70 @@ export class Producto {
 
   constructor(){
     this.getProductos()
+    this.cargarCategorias()
   }
   productos = signal([]);
+  productDialog = signal(false);
+  producto = signal({imagen: '', nombre:"", descripcion: '', unidad_medida: ''});
+  submitted = signal<boolean>(false);
+  totalRecords= signal(0)
+  loading = signal(false)
+  categorias = signal([])
+
+  productoForm = new FormGroup({
+    nombre: new FormControl('', [Validators.required]),
+    descripcion: new FormControl(''),
+    codigo_barra: new FormControl(''),
+    unidad_medida: new FormControl('UNIDAD'),
+    marca: new FormControl("PRUEBA"),
+    categoria: new FormControl(1),
+    precio_venta_actual: new FormControl("0"),
+    estado: new FormControl(true)
+  });
 
   @ViewChild('dt') dt!: Table;
-  productoService = inject(ProductoService)
+  productoService = inject(ProductoService);
+  categoriaService = inject(CategoriaService);
+
+  cargarDatos(event: any){
+    let page = event.first / event.rows + 1;
+    this.getProductos(page, event.rows);
+  }
+
+  cargarCategorias(){
+    this.categoriaService.funListar().subscribe(
+      (res: any) => {
+        this.categorias.set(res);
+      }
+    )
+  }
 
   // cols!: Column[];
 
-  openNew() {}
+  openNew() {
+    this.productDialog.set(true)
+  }
 
   exportCSV() {
     this.dt.exportCSV();
   }
 
-  getProductos(){
-    this.productoService.funListar().subscribe(
-
+  getProductos(page: number = 1, limit: number = 5){
+    this.loading.set(true);
+    this.productoService.funListar(1,page, limit, "").subscribe(
       (res: any) => {
-        this.productos.set(res.data) 
+        console.log(this.loading());
+        this.productos.set(res.data);
+        this.totalRecords.set(res.total);
+        
+        this.loading.set(false);
+
       },
       (error) => {
 
       }
     )
+    this.loading.set(false);
   }
 
   editProduct(pod:any){
@@ -49,4 +96,18 @@ export class Producto {
   deleteProduct(prod: any){
 
   }
+
+  funGuardarProducto(){
+    console.log(this.producto());
+    this.productoForm.value.codigo_barra = "PROD"+Date.now();
+    this.productoForm.value.precio_venta_actual = this.productoForm.value.precio_venta_actual+""
+    this.productoService.funGuardar(this.productoForm.value).subscribe(
+      (res) => {
+        this.getProductos();
+        this.productoForm.reset()
+        this.productDialog.set(false);
+      }
+    )
+  }
+  
 }
